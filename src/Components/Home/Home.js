5a4f8363a12abf265632656c7ba7de88c13cc854
Matheus <matheusbrito1999@hotmail.com>
2021-03-02 21:14:10 -0300

    Vercel

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import styles from './Home.module.css';
-import {ReactComponent as Ghost_green} from '../../Assets/ghost_green.svg'
-import {ReactComponent as Perseguicao} from '../../Assets/perseguicao.svg'
-import { Link } from 'react-router-dom';
+import Ghost_green from '../../Assets/ghost_green.svg'
+import Perseguicao from '../../Assets/perseguicao.svg'
 import Button from '../Button/Button';
 
 export default function Home (){
@@ -11,15 +10,15 @@ export default function Home (){
         <div className={styles.container_roxo}>
             <h1 className={styles.title}><span>T</span>ime<span>T</span>o<span>P</span>lay</h1>
             <section className={styles.oldwoman}>
-                <Ghost_green/>
+                <img src={Ghost_green} alt="Ghost_green"/>
                 <Button> Old-Woman</Button>
             </section>
             <section className={styles.minesweeper}>
-                <Ghost_green/>
+                <img src={Ghost_green} alt="Ghost_green"/>
                 <Button> MinesWeeper</Button>
             </section>
             <footer className={styles.animeLeft}>
-                <Perseguicao/>
+                <img src={Perseguicao} alt="Ghost_green"/>
             </footer>
         </div>
     );
