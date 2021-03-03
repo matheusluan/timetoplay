eeaa1f202fb9099f401e619c4ba5e00560335635
Matheus <matheusbrito1999@hotmail.com>
2021-03-03 20:13:44 -0300

    arruma o botao

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -12,11 +12,11 @@ export default function Home (){
             <h1 className={styles.title}><span>T</span>ime<span>T</span>o<span>P</span>lay</h1>
             <section className={styles.oldwoman}>
                 <img src={Ghost_green} alt="Ghost_green"/>
-                <Button><Link to="/oldwoman">Old-Woman</Link></Button>
+                <Link to="/oldwoman"> <Button>Old-Woman</Button></Link>
             </section>
             <section className={styles.minesweeper}>
                 <img src={Ghost_green} alt="Ghost_green"/>
-                <Button><Link to="/mineswweeper">MinesWeeper</Link> </Button>
+                <Link to="/mineswweeper"><Button>MinesWeeper </Button></Link>
             </section>
             <footer className={styles.animeLeft}>
                 <img src={Perseguicao} alt="Ghost_green"/>
