2faaa56c35c10d60f856bff7c34536344f07a7a1
Matheus <matheusbrito1999@hotmail.com>
2021-03-26 20:57:11 -0300

    agr foi 3x

--- src/Components/Quadro/Quadro.js
+++ src/Components/Quadro/Quadro.js
@@ -1,7 +1,5 @@
 import React from "react";
 import styles from './Quadro.module.css';
-import Ghost_purple from '../../Assets/ghost_purple.svg';
-import Pacman_green from '../../Assets/pacman_green.svg';
 import Celula from "../Celula/Celula";
 
 const Quadro = ({ quadrados, onClick, ...props}) => {
